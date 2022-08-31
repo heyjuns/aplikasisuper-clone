0068017d71a9c52d81a506d1801e247f35601f5d
Juanda <heyjuns@icloud.com>
2022-08-31 12:04:42 +0700

    refactor menu item

---
+++ src/app/shared-components/menu-item/menu-item.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ListHelper } from 'src/app/tabs/tabs';
+
+@Component({
+  selector: 'app-menu-item',
+  templateUrl: './menu-item.component.html',
+  styleUrls: ['./menu-item.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+  ]
+})
+export class MenuItemComponent implements OnInit {
+  @Input() title: string;
+  @Input() menuItems: ListHelper[];
+  constructor() { }
+
+  ngOnInit() { }
+
+}
--- src/app/tabs/beranda/beranda.component.ts
+++ src/app/tabs/beranda/beranda.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { DompetSuperStatusComponent } from 'src/app/shared-components/dompet-super-status/dompet-super-status.component';
+import { MenuItemComponent } from 'src/app/shared-components/menu-item/menu-item.component';
 import { OverlapToolbarComponent } from 'src/app/shared-components/overlap-toolbar/overlap-toolbar.component';
+import { ListHelper } from '../tabs';
 import { BerandaHeaderComponent } from './beranda-header/beranda-header.component';
 import { ImageSlideshowComponent } from './image-slideshow/image-slideshow.component';
 
@@ -10,15 +12,49 @@ import { ImageSlideshowComponent } from './image-slideshow/image-slideshow.compo
   templateUrl: './beranda.component.html',
   styleUrls: ['./beranda.component.scss'],
   standalone: true,
-  imports:[
+  imports: [
     IonicModule,
     BerandaHeaderComponent,
     DompetSuperStatusComponent,
     OverlapToolbarComponent,
-    ImageSlideshowComponent
+    ImageSlideshowComponent,
+    MenuItemComponent,
   ]
 })
 export class BerandaComponent implements OnInit {
+  listOfProductCategory: ListHelper[] = [
+    {
+      icon: 'apps',
+      name: 'Semua Kategori',
+    },
+    {
+      icon: 'camera',
+      name: 'Kecantikan',
+    },
+    {
+      icon: 'diamond',
+      name: 'Perawatan Tubuh'
+    }
+  ];
+  listOfSuperDigital: ListHelper[] = [
+    {
+      icon: 'desktop',
+      name: 'Pulsa & Paket Data'
+    },
+    {
+      icon:'flash',
+      name:'Listrik PLN'
+    },
+    {
+      icon:'wine',
+      name:'Air PDAM'
+    },
+    {
+      icon:'medkit',
+      name:'BPJS Kesehatan'
+    },
+
+  ];
 
   constructor() { }
 
